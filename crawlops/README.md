1d9073feecb8e53f228f15bc7f276d5f03bbf2fd
Madhur Dixit <dmadhur6@gmail.com>
2022-08-22 14:56:38 +0530

    Crawlops readme

---
+++ crawlops/README.md
@@ -0,0 +1,7 @@
+# Crawl Page Opertaions
+
+To achieve the macro vision of Solana StackExchange XP System this POC is important. 
+
+The **crawlops** gives insight on how can we read question data from Solana StackExchange webpage by using BeautifulSoup.
+
+* The `crawlpage.py` has a script which crawls the given URL solana stackExchange page and prints `Question Title`, `Question Upvotes`, `Number Of Answers`, `UNIX timestamp of when the question was created`
--- crawlops/crawlpage.py
+++ crawlops/crawlpage.py
@@ -1,28 +1,37 @@
-
+# Importing Libraries
 from bs4 import BeautifulSoup
 from lxml import etree
 import requests
 import dateutil.parser as dp
   
-  
+# Solana StackExchange URL
 URL = "https://solana.stackexchange.com/questions/2306/are-wallet-addresses-created-or-are-they-discovered"
-  
+
+# Headers
 HEADERS = ({'User-Agent':
             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
             (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',\
             'Accept-Language': 'en-US, en;q=0.5'})
   
+# Getting the webpage HTML
 webpage = requests.get(URL, headers=HEADERS)
+
+# Creating a soup object
 soup = BeautifulSoup(webpage.content, "html.parser")
 dom = etree.HTML(str(soup))
+
+# Extracting Question Title
 print("Question Title -",dom.xpath('//*[@id="question-header"]/h1/a')[0].text)
 
+# Extracting Question Upvote Count
 questionUpvoteCount = dom.xpath('(//*[@itemprop="upvoteCount"])[1]')[0].text.strip()
 print("Question Upvotes -",int(questionUpvoteCount))
 
+# Extracting Number of answers to question
 numAnswers = int(dom.xpath('//*[@id="answers-header"]/div/div[1]/h2/span')[0].text.strip())
 print("Number Of Answers -", numAnswers)
 
+# Extracting Date Created
 qelem = soup.find(attrs={"itemprop" : "dateCreated"})
 print("Question Created on (UNIX timestamp)-",int(dp.parse(qelem['datetime']).timestamp()))
 
